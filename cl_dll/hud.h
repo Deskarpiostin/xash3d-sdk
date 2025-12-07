c25e3097a4b10202500c14c03d930e6627a47656
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-12-07 22:03:37 +0800

    Change all color definitions to white

--- cl_dll/hud.h
+++ cl_dll/hud.h
@@ -22,9 +22,9 @@
 #pragma once
 #if !defined(HUD_H)
 #define HUD_H
-#define RGB_YELLOWISH 0x005F5FFF //95,95,255
-#define RGB_REDISH 0x00FF1010 //255,16,16
-#define RGB_GREENISH 0x0000A000 //0,160,0
+#define RGB_YELLOWISH 0x00FFFFFF //95,95,255
+#define RGB_REDISH 0x00FFFFFF //255,16,16
+#define RGB_GREENISH 0x00FFFFFF //0,160,0
 
 #include "wrect.h"
 #include "cl_dll.h"
